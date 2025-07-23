13d77ef2fab87e3fc5615fe0743923d5cfe2b074
kslee@u2ware.com <kslee@u2ware.com>
2025-07-23 17:42:24 +0900

    ocpp-boot-server

--- src/test/java/io/u2ware/ocpp/server/ApplicationTests.java
+++ src/test/java/io/u2ware/ocpp/server/ApplicationTests.java
@@ -5,16 +5,12 @@ import org.apache.commons.logging.LogFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.context.ApplicationContext;
-import org.springframework.web.socket.WebSocketHandler;
 
 import io.u2ware.ocpp.config.WebSocketHandlerInvoker;
 import io.u2ware.ocpp.v1_6.messaging.CentralSystem;
 import io.u2ware.ocpp.v1_6.messaging.ChargePoint;
 import io.u2ware.ocpp.v1_6.messaging.Specification;
-import io.u2ware.ocpp.v1_6.messaging.SpecificationAction;
-import io.u2ware.ocpp.v1_6.messaging.SpecificationSendingOperations;
 import io.u2ware.ocpp.v1_6.messaging.SpecificationSendingTemplate;
 
 
@@ -43,6 +39,16 @@ class ApplicationTests {
 
 
 		logger.info("2===================");			
+		for(Specification s : Specification.usecases(server)) {
+			serverTemplate.convertAndSend(s.message());
+			Thread.sleep(500);
+		}
+
+		logger.info("3===================");			
+		for(Specification s : Specification.usecases(client)) {
+			clientTemplate.convertAndSend(s.message());
+			Thread.sleep(500);
+		}		
 
 		// SpecificationAction a1 = Specification.InitiatedByCentralSystem.DataTransfer.message();
 		// serverTemplate.convertAndSend(a1);
@@ -54,20 +60,10 @@ class ApplicationTests {
 		// Thread.sleep(1000);
 
 
-		logger.info(Specification.Core.DataTransfer.category());
-		logger.info(Specification.InitiatedByCentralSystem.DataTransfer.category());
+		// logger.info(Specification.Core.DataTransfer.category());
+		// logger.info(Specification.InitiatedByCentralSystem.DataTransfer.category());
 
 
-		// for(Specification s : Specification.usecases(server)) {
-		// 	serverTemplate.convertAndSend(s.message());
-		// 	Thread.sleep(500);
-		// }
-
-		// logger.info("3===================");			
-		// for(Specification s : Specification.usecases(client)) {
-		// 	clientTemplate.convertAndSend(s.message());
-		// 	Thread.sleep(500);
-		// }		
 
 	}
 }
