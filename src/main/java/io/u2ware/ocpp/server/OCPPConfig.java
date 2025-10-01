d8d323041b7c9ab362444b599bb34e08c08ce71c
u2waremanager@gamil.com <u2waremanager@gamil.com>
2025-10-01 16:27:06 +0900

    ocpp-boot-server

--- src/main/java/io/u2ware/ocpp/server/OCPPConfig.java
+++ src/main/java/io/u2ware/ocpp/server/OCPPConfig.java
@@ -1,14 +1,31 @@
 package io.u2ware.ocpp.server;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.u2ware.ocpp.config.EnableOcppServer;
+import io.u2ware.ocpp.config.OcppAttributes;
 import io.u2ware.ocpp.OCPPVersion;
 
 @Configuration
-@EnableOcppServer(version = OCPPVersion.V2_1, uri="/ocpp")
-// @EnableOcppServer(version = OCPPVersion.V2_0_1, uri="/ocpp")
-// @EnableOcppServer(version = OCPPVersion.V1_6, uri="/ocpp")
-public class OCPPConfig {
+@EnableOcppServer(version = OCPPVersion.V2_1)
+// @EnableOcppServer(version = OCPPVersion.V2_0_1)
+// @EnableOcppServer(version = OCPPVersion.V1_6)
+public class OCPPConfig implements WebMvcConfigurer{
     
+
+    @Autowired
+    protected OcppAttributes ocppAttributes;
+
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        String console = ocppAttributes.getOcppBootConsole();
+        String viewName = String.format("redirect:%s", console);
+        registry.addViewController("/").setViewName(viewName);
+    }    
+
+
 }
